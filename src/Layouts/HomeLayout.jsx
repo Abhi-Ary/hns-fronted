5c6b07c7fbc536ec36b8636d62516bc2dc38398d
Abhishek Kumar <abhishej7983@gmail.com>
2024-03-08 17:52:45 +0530

    Added login signup button on side bar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,23 +1,37 @@
-import {FiMenu} from 'react-icons/fi';
-import {AiFillCloseCircle} from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { FiMenu } from 'react-icons/fi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../Components/Footer';
-
+import { useDispatch, useSelector } from 'react-redux';
 
 function HomeLayout(props) {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    function changeWidth(){
+    // for checking if user logged in
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    // for displaying options according to the role
+    const role = useSelector((state) => state?.auth?.role);
+
+    function changeWidth() {
         const drawerSide = document.getElementsByClassName("drawer-side");
         drawerSide[0].style.width = 'auto';
     }
 
-    function hideDrawer(){
+    function hideDrawer() {
         const element = document.getElementsByClassName("drawer-toggle");
         element[0].checked = false;
-       
+
         const drawerSide = document.getElementsByClassName("drawer-side");
         drawerSide[0].style.width = '0';
     }
+
+    function handleLogout(e) {
+        e.preventDefault();
+        navigate("/");
+    }
+
     return (
         <div className="min-h-[90vh]">
             <div className="drawer absolute left-0 z-50 w-fit">
@@ -25,15 +39,14 @@ function HomeLayout(props) {
                 <div className="drawer-content">
                     <label htmlFor="my-drawer" className="cursor-pointer relative">
                         <FiMenu
-                            onClick={changeWidth} 
+                            onClick={changeWidth}
                             size={"32px"}
                             className='font-bold text-white m-4'
                         />
                     </label>
                 </div>
                 <div className='drawer-side w-0'>
-                    <label htmlFor="my-drawer" className='drawer-overlay'>
-                    </label>
+                    <label htmlFor="my-drawer" className='drawer-overlay'></label>
                     <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
                         <li className='w-fit absolute right-2 z-50'>
                             <button onClick={hideDrawer}>
@@ -41,29 +54,61 @@ function HomeLayout(props) {
                             </button>
                         </li>
 
-                        <li>
-                            <Link to = "/">Home</Link>
-                        </li>
+                        
+                        <div className="flex flex-col space-y-2">
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
 
-                        <li>
-                            <Link to = "/hospitals">All Hospital</Link>
-                        </li>
+                            {isLoggedIn && role === 'ADMIN' && (
+                                <li>
+                                    <Link to="/admin/dashboard"> Admin DashBoard</Link>
+                                </li>
+                            )}
 
-                        <li>
-                            <Link to = "/contact">Contact Us</Link>
-                        </li>
+                            <li>
+                                <Link to="/hospitals">All Hospital</Link>
+                            </li>
 
-                        <li>
-                            <Link to = "/about">About Us</Link>
-                        </li>
+                            <li>
+                                <Link to="/contact">Contact Us</Link>
+                            </li>
+
+                            {/* <li>
+                                <Link to="/about">About Us</Link>
+                            </li> */}
+
+                            {!isLoggedIn && (
+                                <li className="flex justify-center space-x-4">
+                                    <div className="w-full flex items-center justify-center">
+                                        <button className='btn btn-primary px-8 py-4 font-semibold rounded-md'>
+                                            <Link to="/login">Login</Link>
+                                        </button>
+                                        <button className='btn btn-secondary px-8 py-4 font-semibold rounded-md'>
+                                            <Link to="/signup">Signup</Link>
+                                        </button>
+                                    </div>
+                                </li>
+                            )}
+
+                            {isLoggedIn && (
+                                <li className='flex justify-center space-x-4'>
+                                    <div className="w-full flex items-center justify-center">
+                                        <button className='btn btn-primary px-8 py-4 font-semibold rounded-md'>
+                                            <Link to="/user/profile">Profile</Link>
+                                        </button>
+                                        <button className='btn btn-secondary px-8 py-4 font-semibold rounded-md'>
+                                            <Link onClick={handleLogout}>Logout</Link>
+                                        </button>
+                                    </div>
+                                </li>
+                            )}
+                        </div>
                     </ul>
-
                 </div>
             </div>
-
-             { props.children }
-             <Footer />
-
+            {props.children}
+            <Footer />
         </div>
     );
 }
